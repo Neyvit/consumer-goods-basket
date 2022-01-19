5b4ceb408fbf97afa2633a06b47b209f82a62311
Komlev Vitaly <successfullproject@gmail.com>
2022-01-20 02:45:06 +0300

    Initial commit

---
+++ src/test/java/pro/sky/java/course2/consumergoodsbasket/ConsumerGoodsBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.consumergoodsbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerGoodsBasketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
